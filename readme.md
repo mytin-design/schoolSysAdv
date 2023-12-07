1bb7401e6f7e069181616156bcd107dec7a6577c
mytin-design <denmytin@gmail.com>
2023-12-07 14:58:20 +0300

    new

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # $This model is the prototype for the Consolata School Marks entering system.
 
 It may perform other functions such as registering students.
+
+Add a way to add a new on the list
--- script.js
+++ script.js
@@ -48,6 +48,7 @@ let gradebox = document.getElementById("studentGradebox");
 let dataT = document.getElementById("dataTable");
 let openRegistry = () => {
     gradebox.style.display = "none";
+    entermarks.style.display = 'none';
     regbox.style.display = "block";
     document.querySelector(".regtitle").innerHTML = "Register";
     document.querySelector(".rgshow").innerHTML = "";
@@ -70,25 +71,31 @@ let closeRegistry = () => {
         rconfirm.style.zIndex = "10";
 }
 
-let rgtxt = document.querySelector(".rgshow");
 
 let openMarksbox = () => {
     regbox.style.display = "none";
     gradebox.style.display = "block";
+    entermarks.style.display = 'none';
     document.querySelector(".regtitle").innerHTML = "Grade";
+    let rgtxt = document.querySelector(".rgshow");
     rgtxt.style.display = "none";
     document.querySelector(".performance").innerHTML = "Grade Student";
 }
 
 let enterMarksbox = () => {
-    regbox.style.display = 'none';
+    regbox.style.display = "none";
     gradebox.style.display = "none";
+    entermarks.style.display = 'block';
     document.querySelector(".regtitle").innerHTML = "Marks";
+    let rgtxt = document.querySelector(".rgshow");
     rgtxt.style.display = "none";
     document.querySelector(".performance").innerHTML = "Enter Marks";
+
 }
 
 let closeGradebox = () => {
+
+
     gradebox.style.display = "block"; //do not close
 }
 
@@ -274,3 +281,47 @@ let confirmRYes = () => {
 }
 
 
+//ENTER MARKS JS
+
+//get all inputs
+let mathmark = document.getElementById("math");
+let engmark = document.getElementById("eng");
+let kiswmark = document.getElementById("kisw");
+let sciemark = document.getElementById("scie");
+let inshamark = document.getElementById("insha");
+let compmark = document.getElementById("comp");
+
+let allSubjects = [mathmark, engmark, kiswmark, sciemark, inshamark, compmark];
+
+//get output buttons
+let outbtn1 = document.getElementById("subject_output_btn1");
+let outbtn2 = document.getElementById("subject_output_btn2");
+
+
+//get output boxes
+let totalmarks = document.getElementById("total_marks");
+let total_subjs = document.getElementById("total_subjs");
+
+let lowerout = document.querySelector('.lower_output');
+
+//calculate total
+
+
+outbtn1.addEventListener('click', function givetotalMarks() {
+    if(mathmark !== null && engmark !== null && sciemark !== null && kiswmark !== null & inshamark !== null && compmark !== null) {
+        //change typeof input from string to number -integer
+        let overallMarks = parseInt(mathmark.value) + parseInt(engmark.value) + parseInt(kiswmark.value) + parseInt(sciemark.value) + parseInt(inshamark.value) + parseInt(compmark.value);
+        return totalmarks.innerHTML = overallMarks + " Marks.";
+    }else {
+        let lowerouter = lowerout.innerHTML = '*Insert all marks';
+        
+        lowerouter.style.color = "#f00";
+    }     
+
+});
+
+
+outbtn2.addEventListener('click', function showsubjectsNum() {
+  let outerme = allSubjects.length;  
+  total_subjs.innerHTML = outerme + " Subjects.";
+})
